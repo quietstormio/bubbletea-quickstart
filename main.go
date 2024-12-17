1a4dbb274f8b6e04b375d76e94acfdb62407cbc7
Dione Marshall <dmarshall94@yahoo.com>
2024-12-16 22:19:15 -0500

    add comments

--- main.go
+++ main.go
@@ -9,25 +9,30 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+// Variables that hold styles for the counter and title
 var (
 	counterStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFA500"))
 	titleStyle   = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#FFFFFF"))
 )
 
+// Define the model
 type model struct {
 	counter int
 }
 
+// Give the model an initial value
 func initialModel() model {
 	return model{
 		counter: 0,
 	}
 }
 
+// Init command to run when the program starts
 func (m model) Init() tea.Cmd {
 	return nil
 }
 
+// Function to clear the screen when the user quits the program
 func clearScreen() {
 	cmd := exec.Command("clear") // Use "cls" for Windows
 	cmd.Stdout = os.Stdout
